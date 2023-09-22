be5002ea4c4d6764ca647da7811c6b600f3b349f
KimDoUk <zjaxj230@naver.com>
2023-09-22 18:16:43 +0900

    [feat] form UI 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,6 +9,17 @@ type Props = {
 }
 
 function App({value, onIncrement, onDecrement}: Props) {
+
+  const [todoValue, setTodoValue] = useState("");
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value);
+  }
+  const addTodo = (e: React.FormEvent<HTMLFormElement>) =>{
+    e.preventDefault();
+    setTodoValue("");
+
+  }
+
   return (
     <div className="App">
       {/* Clicked: {value}times */}
@@ -17,7 +28,11 @@ function App({value, onIncrement, onDecrement}: Props) {
       </button>
       <button onClick={onDecrement}>
         -
-      </button>       
+      </button> 
+      <form onSubmit={addTodo}>
+        <input type="text" value={todoValue} onChange={handleChange}/>
+        <input type="submit"/>
+      </form>      
     </div>
   );
 }
